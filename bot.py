91083668035fc9b1366f513d66be8a46d8a0fa79
Alex <sasha.knopka@gmail.com>
2025-10-02 11:43:32 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -193,6 +193,5 @@ async def main():
     await app.run_polling()
 
 if __name__ == "__main__":
-    import nest_asyncio
-    nest_asyncio.apply()
-    asyncio.run(main())
+    asyncio.get_event_loop().create_task(reminder_checker(app))
+    app.run_polling()
